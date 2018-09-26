ebcbd2c2f715217c0040ffaf10557542333c25cc
Alexcyfy <acollanton@mail.csuchico.edu>
2018-09-26 16:10:43 -0700

    Quits when Q is pressed and loops menu

--- main.cpp
+++ main.cpp
@@ -1,3 +1,4 @@
+//Author:Alex Collantoni
 #include<iostream>
 #include<string>
 using namespace std;
@@ -7,13 +8,21 @@ int main()
   string list[5]; //array of 5 strings
   int numItems = 0;
   char input;
-
+  int start;
+  start = 1;
+  while(start =1)
+ {
   cout<<"\n==GROCERY LIST MANAGER==";
   cout<<"\nEnter your choice: ";
   cout<<"\n (A)dd an item";
   cout<<"\n (Q)uit";
   cout<<"\nYour choice (A/Q): ";
   cin>>input;
-
+  if(input == 'Q')
+  {
+  start = 0;
+  break;
+  }
+ }
   return 0;
 }
